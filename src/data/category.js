21fab78f554dfda56bfcaad91c8fb55be6318903
jddach77 <jddach@gmail.com>
2021-08-11 11:27:57 +0100

    Update category.js
    removed a few languages

--- src/data/category.js
+++ src/data/category.js
@@ -1,16 +1,11 @@
 const categories = [
 	{ value: "English", label: "en" },
-	{ value: "Hindi", label: "hi" },
-	{ value: "Spanish", label: "es" },
 	{ value: "French", label: "fr" },
-	{ value: "Japanese", label: "jp" },
-	{ value: "Russian", label: "ru" },
-	{ value: "German", label: "de" },
 	{ value: "Italian", label: "it" },
-	{ value: "Korean", label: "ko"},
-	{ value: "Brazilian Portuguese", label: "pt-BR" },
-	{ value: "Arabic", label: "ar" },
-	{ value: "Turkish", label: "tr" },
+	{ value: "Japanese", label: "jp" },	
+	{ value: "German", label: "de" },
+	{ value: "Portuguese", label: "pt-BR" },
+	{ value: "Spanish", label: "es" },
 ];
 
 export default categories;
